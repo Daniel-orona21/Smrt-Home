0220e6725a48cc76980fc1bb6fc81909e87d3214
Alejandro Daniel Lopez Orona <alejandro_3041220281@utd.edu.mx>
2024-02-03 21:30:03 -0600

    main

--- App.js
+++ App.js
@@ -61,6 +61,8 @@ export default function App() {
             headerTintColor: 'black', 
           }}
         >
+          
+          
           {({ navigation }) => (
             <HomeScreen
               navigation={navigation}
